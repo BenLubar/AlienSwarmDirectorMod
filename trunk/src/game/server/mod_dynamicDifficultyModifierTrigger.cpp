5bdae74c7fed72e09939c3e0dc64a0478c90f7d7
ppittle <ppittle@2c9b7856-6c2d-475a-89f6-b655c9bcaa01>
2011-06-05 23:11:18 +0000

    git-svn-id: https://subversion.assembla.com/svn/alienswarmdirectormod@24 2c9b7856-6c2d-475a-89f6-b655c9bcaa01

---
+++ trunk/src/game/server/mod_dynamicDifficultyModifierTrigger.cpp
@@ -0,0 +1,75 @@
+#include "cbase.h"
+#include "triggers.h"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+class CModDynamicDifficultyModifierTrigger : public CLogicalEntity
+{
+public:
+	DECLARE_CLASS ( CModDynamicDifficultyModifierTrigger, CLogicalEntity);
+
+	DECLARE_DATADESC();
+
+	//Constructor
+	CModDynamicDifficultyModifierTrigger()
+	{
+		m_bHasCheckedDifficulty = false;
+		m_bDifficultyThresholdReached = false;
+		m_iMinDifficultyThreshold = 1;
+		m_iMaxDifficultyThreshold = 3;
+	}
+
+	//Input function
+	void CheckDifficultyThreshold (inputdata_t &inputData);
+	void ParseDifficultyThreshold();
+
+private:
+		bool m_bHasCheckedDifficulty;
+		bool m_bDifficultyThresholdReached;
+		//1 = easy, 2 = medium, 3 = hard
+		int m_iMinDifficultyThreshold;
+		int m_iMaxDifficultyThreshold;
+
+		COutputEvent m_OnGetDifficultyThreshold;
+};
+
+LINK_ENTITY_TO_CLASS(mod_dynaimic_difficulty_modifier_trigger, CModDynamicDifficultyModifierTrigger);
+
+BEGIN_DATADESC (CModDynamicDifficultyModifierTrigger)
+	// For save/load
+	DEFINE_FIELD(m_bHasCheckedDifficulty, FIELD_BOOLEAN),
+	DEFINE_FIELD(m_bDifficultyThresholdReached, FIELD_BOOLEAN),
+
+	//Link Member Variable to a Hammer keyvalue
+	DEFINE_KEYFIELD(m_MinDifficultyThreshold, FIELD_INTEGER, "minDifficultyThreshold"),
+	DEFINE_KEYFIELD(m_iMaxDifficultyThreshold, FIELD_INTEGER, "maxDifficultyThreshold"),
+
+	//Links out input name from Hammer to our input member
+	DEFINE_INPUTFUNC(FIELD_VOID, "CheckDifficulty", CheckDifficultyThreshold),
+
+	//Links our output member variable to the output name
+	DEFINE_OUTPUT(m_OnGetDifficultyThreshold, "OnGetDifficultyThreshold")
+
+END_DATADESC()
+
+void  CModDynamicDifficultyModifierTrigger::ParseDifficultyThreshold()
+{
+	if (!m_bHasCheckedDifficulty)
+	{
+		//TODO: Actually check AI system instead of using random
+		int marinesPerformance = (rand() % 3) + 1;
+
+		m_bDifficultyThresholdReached = (m_iDifficultyThreshold <= marinesPerformance);
+
+		m_bHasCheckedDifficulty = true;
+	}
+}
+
+void CModDynamicDifficultyModifierTrigger::CheckDifficultyThreshold (inputdata_t &inputData)
+{
+	ParseDifficultyThreshold();
+
+	if (m_bDifficultyThresholdReached)
+		m_OnGetDifficultyThreshold.FireOutput(inputData.pActivator, this);
+}
