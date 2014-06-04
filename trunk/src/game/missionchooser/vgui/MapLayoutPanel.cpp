cedea545314d2df619dcd14a95531f6d09600409
Ben Lubar <ben.lubar@gmail.com>
2014-06-04 17:35:29 -0500

    CRASH: fix inversion of parameters

--- trunk/src/game/missionchooser/vgui/MapLayoutPanel.cpp
+++ trunk/src/game/missionchooser/vgui/MapLayoutPanel.cpp
@@ -213,7 +213,7 @@ void CMapLayoutPanel::AddRoom( const CRoomTemplate *pTemplate, int iTileX, int i
 	if ( !g_pTileGenDialog )
 		return;
 
-	CRoom *pRoom = new CRoom( GetMapLayout(), g_pTileGenDialog->m_pCursorTemplate->GetFullName(), g_pTileGenDialog->m_pCursorTemplate->m_iszLevelTheme, iTileX, iTileY );
+	CRoom *pRoom = new CRoom(GetMapLayout(), g_pTileGenDialog->m_pCursorTemplate->m_iszLevelTheme, g_pTileGenDialog->m_pCursorTemplate->GetFullName(), iTileX, iTileY);
 	CreateRoomUIPanel( pRoom );
 }
 
