0f0238db2566da3e8f66b4eb983c374324d9695f
Ben Lubar <ben.lubar@gmail.com>
2014-07-05 11:49:08 -0500

    GAMEMODE: fix medkit being usable when empty

--- src/game/shared/swarm/asw_weapon_medkit_shared.cpp
+++ src/game/shared/swarm/asw_weapon_medkit_shared.cpp
@@ -90,6 +90,9 @@ void CASW_Weapon_Medkit::SelfHeal()
 		if (pMarine->GetFlags() & FL_FROZEN)	// don't allow this if the marine is frozen
 			return;
 
+		if (m_iClip1 <= 0) // we're out of ammo!
+			return;
+
 		// MUST call sound before removing a round from the clip of a CMachineGun
 		WeaponSound(SINGLE);
 
