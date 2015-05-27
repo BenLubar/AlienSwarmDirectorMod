a6dcdd2621742b4ab977028246e94b67269e6ec1
Ben Lubar <ben.lubar@gmail.com>
2015-05-26 20:31:01 -0500

    UI: autocomplete asw_startcampaign console commands

--- src/game/shared/swarm/asw_campaign_commands.cpp
+++ src/game/shared/swarm/asw_campaign_commands.cpp
@@ -2,6 +2,7 @@
 #include "filesystem.h"
 #include "missionchooser/iasw_mission_chooser.h"
 #include "missionchooser/iasw_mission_chooser_source.h"
+#include "../missionchooser/asw_mission_chooser_source_local.h"
 
 // memdbgon must be the last include file in a .cpp file!!!
 #include "tier0/memdbgon.h"
@@ -37,43 +38,99 @@ void ASW_LaunchSaveGame(const char *szSaveName, bool bMultiplayer, bool bChangeL
 #endif
 }
 
-// con command for new campaign
-void ASW_StartNewCampaign_cc( const CCommand &args )
+class CASW_StartNewCampaign : public ICommandCallback, public ICommandCompletionCallback
 {
-	IASW_Mission_Chooser_Source* pSource = missionchooser ? missionchooser->LocalMissionSource() : NULL;
-	if (!pSource)
-		return;
-
-	if ( args.ArgC() < 4 )
+	virtual void CommandCallback( const CCommand &command )
 	{
-		Msg( "Usage: ASW_StartCampaign [CampaignName] [SaveName] [SP|MP]\n   Use 'auto' for SaveName to have an autonumbered save.\n" );
-		return;
+		IASW_Mission_Chooser_Source *pSource = missionchooser ? missionchooser->LocalMissionSource() : NULL;
+		if ( !pSource )
+		{
+			return;
+		}
+
+		if ( command.ArgC() < 4 || ( V_stricmp( command.Arg( 3 ), "MP" ) &&  V_stricmp( command.Arg( 3 ), "SP" ) ) )
+		{
+			Msg( "Usage: ASW_StartCampaign [CampaignName] [SaveName] [SP|MP]\n   Use 'auto' for SaveName to have an autonumbered save.\n" );
+			return;
+		}
+
+		// create a new savegame at the start of the campaign
+		char buffer[MAX_PATH];
+		if ( !V_stricmp( command.Arg( 2 ), "auto" ) )
+		{
+			buffer[0] = '\0';
+		}
+		else
+		{
+			V_strncpy(buffer, command.Arg( 2 ), sizeof(buffer));
+		}
+
+		// Use an arg to detect MP (would be better to check maxplayers here, but we can't?)
+		bool bMulti = !V_stricmp( command.Arg( 3 ), "MP" );
+
+		if ( !pSource->ASW_Campaign_CreateNewSaveGame( buffer, sizeof( buffer ), command.Arg( 1 ), bMulti, NULL ) )
+		{
+			Msg( "Unable to create new save game.\n" );
+			return;
+		}
+
+		// go to the load savegame code
+		ASW_LaunchSaveGame( buffer, bMulti, command.ArgC() == 5 ); // if 4th parameter specified, do a changelevel instead of launching a new map.
 	}
 
-	// create a new savegame at the start of the campaign
-	char buffer[256];
-	if (!Q_strnicmp( args[2], "auto", 4 ))
+	virtual int CommandCompletionCallback( const char *partial, CUtlVector<CUtlString> &commands )
 	{
-		buffer[0] = '\0';
-	}
-	else
-		Q_snprintf(buffer, sizeof(buffer), "%s", args[2]);
+		CASW_Mission_Chooser_Source_Local *pSource = missionchooser ? assert_cast<CASW_Mission_Chooser_Source_Local *>( missionchooser->LocalMissionSource() ) : NULL;
+		if ( !pSource )
+		{
+			return 0;
+		}
 
-	// Use an arg to detect MP (would be better to check maxplayers here, but we can't?)
-	bool bMulti = (!Q_strnicmp( args[3], "MP", 2 ));
-	if (!pSource->ASW_Campaign_CreateNewSaveGame(buffer, sizeof(buffer), args[1], bMulti, NULL))
-	{
-		Msg( "Unable to create new save game.\n" );
-		return;
-	}
+		const char *base = IsClientDll() ? "ASW_StartCampaign " : "ASW_Server_StartCampaign ";
 
-	// go to the load savegame code
-	ASW_LaunchSaveGame(buffer, bMulti, args.ArgC() == 5);	// if 3rd parameter specified, do a changelevel instead of launching a new
-}
+		// using strlen instead of V_strlen so hopefully the compiler can precompute it.
+
+		if ( V_strncmp( partial, base, strlen( base ) ) )
+		{
+			// No space after the command.
+			return 0;
+		}
+
+		const char *pszCampaign = partial + strlen( base );
+
+		if ( V_strstr( pszCampaign, pszCampaign[0] == '"' ? "\"" : " " ) )
+		{
+			// Campaign name finished - we're done.
+			return 0;
+		}
+
+		if ( pszCampaign[0] == '"' )
+		{
+			// Skip opening quote.
+			pszCampaign++;
+		}
+
+		int nSearchLength = V_strlen( pszCampaign );
+		FOR_EACH_VEC( pSource->m_CampaignList, i )
+		{
+			if ( !V_strnicmp( pSource->m_CampaignList[i].szMapName, pszCampaign, nSearchLength ) )
+			{
+				CUtlString command( base );
+				command += pSource->m_CampaignList[i].szMapName;
+				command.SetLength( command.Length() - strlen( ".txt" ) );
+				command += " ";
+				commands.AddToTail( command );
+			}
+		}
+
+		return commands.Count();
+	}
+};
+static CASW_StartNewCampaign ASW_StartNewCampaign_cc;
 #ifdef CLIENT_DLL
-ConCommand ASW_StartCampaign( "ASW_StartCampaign", ASW_StartNewCampaign_cc, "Starts a new campaign game", FCVAR_DONTRECORD );
+ConCommand ASW_StartCampaign( "ASW_StartCampaign", &ASW_StartNewCampaign_cc, "Starts a new campaign game", FCVAR_DONTRECORD, &ASW_StartNewCampaign_cc );
 #else
-ConCommand ASW_Server_StartCampaign( "ASW_Server_StartCampaign", ASW_StartNewCampaign_cc, "Server starts a new campaign game", FCVAR_DONTRECORD );
+ConCommand ASW_Server_StartCampaign( "ASW_Server_StartCampaign", &ASW_StartNewCampaign_cc, "Server starts a new campaign game", FCVAR_DONTRECORD, &ASW_StartNewCampaign_cc );
 #endif
 
 // con command for loading a savegame
